895a6ee5d78d14e1bd9315dc8914724482e95628
DavePearce <david.pearce@consensys.net>
2022-09-13 13:25:12 +1200

    Support Gas Refund #282
    This puts through support for giving back gas after a successful call.
    Note, however, that an unsuccessful call fails.

--- src/main/java/dafnyevm/DafnyEvm.java
+++ src/main/java/dafnyevm/DafnyEvm.java
@@ -390,7 +390,6 @@ public class DafnyEvm {
 	 * @return
 	 */
 	private DafnyEvm.State<?> call(int depth) {
-		System.out.println("DEPTH: " + depth + ", " + Hex.toHexString(gas));
 		if (depth >= 1024) {
 			// The Yellow Paper specifies a maximum depth of 1024.
 			return State.from(depth, tracer, new EvmState_Compile.State_INVALID(new Error_CALLDEPTH__EXCEEDED()));
@@ -422,8 +421,6 @@ public class DafnyEvm {
 					} else {
 						r = createContinue((State.CreateContinue)r, depth);
 					}
-					System.out.println(">>> " + r);
-
 				}
 				return r;
 			}
