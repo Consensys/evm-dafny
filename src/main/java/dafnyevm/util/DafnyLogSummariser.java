12737be996e3c6c7b6530c291c981c3fc8c97655
DavePearce <david.pearce@consensys.net>
2023-03-08 16:15:37 +1300

    Support command-line arguments in summariser
    This adds some simple command-line arguments to the log summariser.

--- src/main/java/dafnyevm/util/DafnyLogSummariser.java
+++ src/main/java/dafnyevm/util/DafnyLogSummariser.java
@@ -22,6 +22,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 class Result {
     public final String name;
     public final long usage;
@@ -38,7 +46,32 @@ class Result {
 }
 
 public class DafnyLogSummariser {
+    private static final Option[] OPTIONS = new Option[] {
+            new Option("entries", true, "Report at most n results"),
+    };
+
+    public static CommandLine parseCommandLine(String[] args) {
+        // Configure command-line options.
+        Options options = new Options();
+        for(Option o : OPTIONS) { options.addOption(o); }
+        CommandLineParser parser = new DefaultParser();
+        // use to read Command Line Arguments
+        HelpFormatter formatter = new HelpFormatter();  // // Use to Format
+        try {
+            return parser.parse(options, args);  //it will parse according to the options and parse option value
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("dafnyevm", options);
+            System.exit(1);
+            return null;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
+        // Parse command-line arguments.
+        CommandLine cmd = parseCommandLine(args);
+        int nResults = Integer.parseInt(cmd.getOptionValue("entries", "10"));
+        //
         FileSystem fs = FileSystems.getDefault();
         ArrayList<Result> results = new ArrayList<>();
         // Process globs given on the command-line.
@@ -53,8 +86,8 @@ public class DafnyLogSummariser {
         // Print out header
         System.out.println("Name" + String.format("%1$" + 76 + "s", "Resource Usage"));
         System.out.println(String.format("%1$" + 80 + "s", "").replace(' ','='));
-        // Print top ten results
-        for(int i=0;i!=Math.min(10,results.size());++i) {
+        // Print top n results
+        for(int i=0;i!=Math.min(nResults,results.size());++i) {
             Result ith = results.get(i);
             System.out.println(ith.toString(80));
         }
