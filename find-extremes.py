35fd8d50ea384807af5b99a1a83af26eda6134c0
Horacio Mijail <hmijail@gmail.com>
2024-03-13 12:29:46 +1000

    find-extremes finished

--- find-extremes.py
+++ find-extremes.py
@@ -5,7 +5,8 @@ Runs measure-complexity with random seeds until it finds logs with Resource Usag
 import argparse
 import csv
 from math import inf
-from os import error, rename
+from os import error, mkdir, rename
+import os
 import random
 import subprocess as sp
 import sys
@@ -15,6 +16,8 @@ import logging as log
 import enum
 from datetime import datetime as dt, timedelta as td
 from quantiphy import Quantity
+# from yaspin import yaspin, Spinner
+# from yaspin.spinners import Spinners
 
 def shell(str, **kwargs):
     """Convenient way to run a shell command and get its exit code, stdout, stderr.."""
@@ -22,77 +25,128 @@ def shell(str, **kwargs):
     #print(r)
     return r
 
+def smag(i) -> str:
+    return f"{Quantity(i):.3}"
+
 parser = argparse.ArgumentParser()
 parser.add_argument("dafnyfile")
 parser.add_argument("displayname")
 parser.add_argument("min", type=Quantity)
 parser.add_argument("max", type=Quantity)
+parser.add_argument("extra_args", nargs='?', default="")
+parser.add_argument("-v", "--verbose", action="count", default=0)
+
 args = parser.parse_args()
 #remember there's also sys.argv
 
-loglevel = "debug"
-numeric_level = getattr(log, loglevel.upper(), None)
-if not isinstance(numeric_level, int):
-    raise ValueError('Invalid log level: %s' % loglevel)
+# loglevel = "debug"
+# numeric_level = getattr(log, loglevel.upper(), None)
+# if not isinstance(numeric_level, int):
+#     raise ValueError('Invalid log level: %s' % loglevel)
+numeric_level = log.WARNING - args.verbose * 10
 log.basicConfig(level=numeric_level,format='%(levelname)s:%(message)s')
 
 
-uint32_max = 4294967295 # the Dafny random seed is expected as a C# uint32
+uint32_max = 4294967295
 random.seed()
-min_ru = inf
-max_ru = -inf
+ru_gmin = inf
+seed_gmin = None
+ru_gmax = -inf
+seed_gmax = None
 Quantity.set_prefs(strip_zeros=False)
+iterations = 0
+logs_directory: str = "TestResults/" + dt.now().strftime('%Y%m%d-%H%M%S')
+os.makedirs(logs_directory)
 
+#with yaspin(Spinner("/-\\|",1000), text="Working...") as spinner:
+#    spinner.hide()
 while True:
+    iterations += 1
     # Better not to simply sweep random-seed from 0 because we'd be repeating that sequence at each new run
     random_seed = random.randint(0, uint32_max)
-    argstring = f"--random-seed {random_seed}"  # --iterations 1"
-    argstring4filename = (argstring + args.displayname).replace("/","").replace("--","").replace(" ","")
+    #argstring = f"--random-seed {random_seed} "
+    argstring = f"--boogie /randomSeed:{random_seed}"
+    #argstring += "--iterations 1 "
+    argstring += args.extra_args
+    argstring4filename = (argstring + args.displayname).replace("/","").replace("--","").replace(" ","").replace(":","")
     d : str = dt.now()
     dstr = d.strftime('%Y%m%d-%H%M%S')
-    filename = "TestResults/" + dstr + "_" + argstring4filename + ".csv"
-    # log.debug(f"filename={filename}")
+    filename = logs_directory + "/" + dstr + "_" + argstring4filename + ".csv"
 
-    res = shell(fr"dafny44 measure-complexity --log-format csv\;LogFileName='{filename}' {argstring} {args.dafnyfile}")
+    #CLI_line: str = fr"dafny44 measure-complexity --log-format csv\;LogFileName='{filename}' {argstring} {args.dafnyfile}"
+    CLI_line: str = fr"dafny44 verify --log-format csv\;LogFileName='{filename}' {argstring} {args.dafnyfile}"
+    log.debug("executing: " + CLI_line)
+    try:
+        res = shell(CLI_line, timeout=10)
+    except sp.TimeoutExpired as te:
+        log.info(f"Timed out:\n{te}")
+        #log.info("executed: " + CLI_line)
+        continue
+
     if res.returncode == 0:
         with open(filename) as csvfile:
             reader = csv.DictReader(csvfile)
-            ru = None
+            rows_num = 0
+            rus: list[int] = []
             for row in reader:
+                rows_num += 1
+                log.debug(f"CSV row {rows_num}")
                 if args.displayname in row['TestResult.DisplayName'] :
-                    if ru == None:
-                        ru = int(row['TestResult.ResourceCount'])
-                    else:
-                        # if we run a single iteration, there should be no multiple rows for a given method/function, right? Though those might happen when something is included or whatever...
-                        log.error("Multiple rows for {arg.displayname}!")
-                        exit(100)
-                    break
-        if ru == None:
+                    # if ru == None:
+                    #     ru = int(row['TestResult.ResourceCount'])
+                    # else:
+                    #     # if we run a single iteration, there should be no multiple rows for a given method/function, right? Though those might happen when something is included or whatever...?
+                    #     log.error(f"File {filename} contains multiple rows for {args.displayname}!")
+                    #     exit(100)
+                    rus.append(int(row['TestResult.ResourceCount']))
+
+        if rus == []:
             log.error("DisplayName not in results!")
             exit(100)
     else:
         print(f"returncode={res.returncode}")
         print(f"stdout={res.stdout}")
         print(f"stderr={res.stderr}")
+        continue
         #exit(res.returncode)
 
-    rus = f"{Quantity(ru):.3}"
-    line = f"seed {random_seed:12} -> {rus:7} RU"
+    rus.sort()
+    rus_min = rus[0]
+    rus_max = rus[-1]
 
-    if ru < min_ru:
-        filename2 = f"{filename}MIN{rus}"
+    rus_range_str = f"{smag(rus_min)}"
+
+    if rus_min != rus_max:
+        rus_range_str += f"..{smag(rus_max)}"
+
+    if rus_min < ru_gmin:
+        filename2 = f"{filename[:-4]}MIN{smag(rus_min)}.csv"
         rename(filename, filename2)
-        min_ru = ru
-        line += "\tMIN"
-    elif ru > max_ru:
-        filename2 = f"{filename}MAX{rus}"
+        ru_gmin = rus_min
+        seed_gmin = random_seed
+    elif rus_max > ru_gmax:
+        filename2 = f"{filename[:-4]}MAX{smag(rus_max)}.csv"
         rename(filename, filename2)
-        max_ru = ru
-        line += "\tMAX"
+        ru_gmax = rus_max
+        seed_gmax = random_seed
 
-    print(line)
+    status = f"iteration {iterations:4}: seed {random_seed:12} -> {len(rus)}/{rows_num} rows, RU {rus_range_str}\t-"
+    status += f"\tmin {smag(ru_gmin)} "
+    if ru_gmin <= args.min:
+        status += "✅"
+    status += f"\tmax {smag(ru_gmax)} "
+    if ru_gmax >= args.max:
+        status += "✅"
 
-    if min_ru <= args.min and max_ru >= args.max:
-        # generate HTML report
+    #spinner.text=line #+ "\n"
+    #spinner.write(line)
+    log.info(status)
+
+    if ru_gmin <= args.min and ru_gmax >= args.max:
+        # TODO generate HTML report
+        #spinner.write(line)
         break
+    else:
+        time.sleep(1)
 
+print(f"Done: {iterations} iterations. Max RU = {ru_gmax} with seed {seed_gmax}. Min RU = {ru_gmin} with seed {seed_gmin}.")
