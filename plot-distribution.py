31513561ffd9f5ec89efe5d3b68f833a98b5db5a
Horacio Mijail <hmijail@gmail.com>
2024-04-09 09:53:03 +1000

    Holoviews

--- plot-distribution.py
+++ plot-distribution.py
@@ -17,10 +17,6 @@ import logging as log
 import os
 import pickle
 from time import sleep
-# from matplotlib.container import BarContainer
-# import matplotlib.pyplot as plt
-# from matplotlib.ticker import EngFormatter
-#from matplotlib.animation import adjusted_figsize
 import numpy as np
 import re
 from datetime import datetime as dt, timedelta as td
@@ -61,10 +57,10 @@ parser.add_argument("-d", "--delta", default=10)
 args = parser.parse_args()
 
 numeric_level = log.WARNING - args.verbose * 10
-log.basicConfig(level=numeric_level,format='%(levelname)s:%(message)s')
+log.basicConfig(level=numeric_level,format='%(asctime)s-%(levelname)s:%(message)s',datefmt='%H:%M:%S')
 
 
-usages: dict[str, list[int]] = {}
+usages: dict[str, list[int]] = {}   # DisplayName : list of RCs
 files = 0
 rows = 0
 # to be un/pickled: [files, rows, usages]
@@ -120,6 +116,35 @@ else:
 
 dns = list(usages.keys())
 m = [l for l in usages.values()]
+maxRC = np.max(m) + 1
+bins = np.linspace(0,maxRC, num=args.nbins)
+bin_size = maxRC / args.nbins
+counts, bins2 = np.histogram(m,bins=bins)
+#bins = 0.5 * (bins[:-1] + bins[1:])
+
+log.info("Selecting traces")
+traces_selected = []
+labels_selected = []
+for k,v in usages.items():
+    minRC = min(v)
+    maxRC = max(v)
+    delta = (maxRC-minRC)/maxRC # difference between max and min
+    line = f"{k}: \t{len(v)} points \t{smag(minRC)} - {smag(maxRC)} \tdiff={delta:.2%}"
+    log.debug(line)
+    if delta < args.delta/100:
+        continue
+
+    print(f"{line}\t PLOTTED")
+    traces_selected.append(v)
+    labels_selected.append(k)
+
+
+# MATPLOTLIB
+
+# from matplotlib.container import BarContainer
+# import matplotlib.pyplot as plt
+# from matplotlib.ticker import EngFormatter
+#from matplotlib.animation import adjusted_figsize
 
 # fig, ax = plt.subplots()
 # #hist, bins, patches = ax.hist(usages, bins=10, edgecolor='black')
@@ -132,12 +157,8 @@ m = [l for l in usages.values()]
 # # for c in counts:
 # #     plt.bar(bins[:-1], c)
 
-
 # #ax.legend()#shadow=True, fancybox=True)
 # #ax.set_yscale('log')
-
-
-
 
 # plt.xlabel('RC')
 # plt.ylabel('occurrences')
@@ -149,101 +170,112 @@ m = [l for l in usages.values()]
 # plt.show()
 
 
+# PLOTLY FIGUREFACTORY
+
+# #DEPRECATED
+# import plotly.figure_factory as ff
+# fig_ff = ff.create_distplot(traces, labels, bin_size=bin_size,show_curve=False)
+
+
+# title = f'{args.nbins} bins, {len(traces)}/{len(usages)} categories'
+# print(title)
+
+# # fig.update_traces({"visible":"legendonly"})
+# fig_ff.update_layout(
+#     title_text=title, # title of plot
+#     xaxis_title_text='Resource Count', # xaxis label
+#     yaxis_title_text='Occurrences', # yaxis label
+#     bargap=0.2, # gap between bars of adjacent location coordinates
+#     bargroupgap=0, # gap between bars of the same location coordinates,
+
+# )
+
+# if args.ylog:
+#     fig_ff.update_yaxes(type="log")
+
+# fig_ff.write_html('ff.html', auto_open=False)
+# #fig_ff.show()
+
+# #del fig_ff
 
 
 
+# # PLOTLY EXPRESS
 
-#fig: Figure = px.histogram(usages, barmode="group",marginal="rug", nbins=50)
-
-maxRC = np.max(m) + 1
-bins = np.linspace(0,maxRC, num=args.nbins)
-bin_size = maxRC / args.nbins
-#counts, bins2 = np.histogram(m,bins=bins)
-#bins = 0.5 * (bins[:-1] + bins[1:])
-
+# # impossibly slow
 # import plotly.express as px
-# fig = px.bar(x=bins[:-1], y=counts, barmode="group")#,labels={'x':'RC', 'y':'occurrences'},)
+# fig_px = px.histogram(usages, x=labels, barmode="group", nbins = args.nbins, marginal="rug")#,labels=labels,)
+# # fig = px.bar(x=bins[:-1], y=counts, barmode="group")#,labels={'x':'RC', 'y':'occurrences'},)
+
+# fig_px.update_layout(
+#     title_text=title, # title of plot
+#     xaxis_title_text='Resource Count', # xaxis label
+#     yaxis_title_text='Occurrences', # yaxis label
+#     bargap=0.2, # gap between bars of adjacent location coordinates
+#     bargroupgap=0, # gap between bars of the same location coordinates,
+# )
+
+# if args.ylog:
+#     fig_px.update_yaxes(type="log")
+# # to set x-axes ticks to limits of bins
+# #fig.update_xaxes(tickvals=np.arange(range_dict.start, range_dict.end+range_dict.size, range_dict.size))
+
+# fig_px.write_html('px.html', auto_open=False)
+# #fig_px.show()
+# #del fig_px
 
 
 
-traces = []
-labels = []
-for k,v in usages.items():
-    minRC = min(v)
-    maxRC = max(v)
-    delta = (maxRC-minRC)/maxRC # difference between max and min
-    print(f"{k}: \t{len(v)} points \t{smag(minRC)} - {smag(maxRC)} \tdiff={delta:.2%}", end="")
-    if delta < args.delta/100:
-        print("")
-        continue
 
-    print("\t PLOTTED")
-    traces.append(v)
-    labels.append(k)
+# HOLOVIEWS
 
-#DEPRECATED
-#import plotly.figure_factory as ff
-#fig = ff.create_distplot(traces, labels, bin_size=bin_size,show_curve=False)
+log.info("Imports")
+import numpy as np
+import pandas as pd
+import holoviews as hv
+import hvplot
+from hvplot import hvPlot
+import hvplot.pandas
+from holoviews import opts
 
-# impossibly slow
-import plotly.express as px
-fig = px.histogram(usages, x=labels, barmode="group", nbins = args.nbins, marginal="rug")#,labels=labels,)
+log.info("hvplot extension")
+hvplot.extension('bokeh')
+log.info("hvplot renderer")
+renderer = hv.renderer('bokeh')
 
-title = f'{args.nbins} bins, {len(traces)}/{len(usages)} categories'
-print(title)
 
-# fig.update_traces({"visible":"legendonly"})
-fig.update_layout(
-    title_text=title, # title of plot
-    xaxis_title_text='Resource Count', # xaxis label
-    yaxis_title_text='Occurrences', # yaxis label
-    bargap=0.2, # gap between bars of adjacent location coordinates
-    bargroupgap=0, # gap between bars of the same location coordinates,
+log.info("creating dataframe")
+dfu = pd.DataFrame(usages)
+dfu = dfu.reset_index()
 
+log.info("hvplot")
+hist = dfu.hvplot(kind='hist',y=labels_selected, xlabel="Resource Count", ylabel="Occurrences", bins=25, responsive=True, height=500, logy=False, ylim=[0.5,1000])
+#hist2 = dfu.hvplot.hist(y=labels_selected, xlabel="Resource Count", ylabel="Occurrences", bins=25)
+violin = dfu.hvplot(kind='violin', y=labels_selected, invert=True, responsive=True, height=200)
+violin.opts(opts.Violin(inner='stick'))
+
+plot = violin + hist
+plot.cols(1)
+plot.opts(
+    opts.Violin(tools=['box_select','lasso_select']),
+    #opts.Histogram(responsive=True, height=500, width=1000),
+    opts.Layout(sizing_mode="scale_both", shared_axes=True, sync_legends=True, shared_datasource=True)
 )
-
-if args.ylog:
-    fig.update_yaxes(type="log")
-
-# to set x-axes ticks to limits of bins
-#fig.update_xaxes(tickvals=np.arange(range_dict.start, range_dict.end+range_dict.size, range_dict.size))
-
-print(fig.to_dict())
-
-# ADD 2 BUTTONS TO TOGGLE LOG/LINEAR Y, but only in the yaxis 1
-# fig.update_layout(
-#     updatemenus=[
-#         dict(
-#             type="buttons",
-#             direction="right",
-#             active=0,
-#             x=0.57,
-#             y=1.2,
-#             buttons=list([
-#                 dict(label="Linear",
-#                      method="update",
-#                      args=[{"visible": [True, False, True, False]},
-#                            {"title": "Yahoo",
-#                             "annotations": []}]),
-#                 dict(label="High",
-#                      method="update",
-#                      args=[{"visible": [True, True, False, False]},
-#                            {"title": "Yahoo High",
-#                             "annotations": high_annotations}]),
-#                 dict(label="Low",
-#                      method="update",
-#                      args=[{"visible": [False, False, True, True]},
-#                            {"title": "Yahoo Low",
-#                             "annotations": low_annotations}]),
-#                 dict(label="Both",
-#                      method="update",
-#                      args=[{"visible": [True, True, True, True]},
-#                            {"title": "Yahoo",
-#                             "annotations": high_annotations + low_annotations}]),
-#             ]),
-#         )
-#     ])
+ls = hv.link_selections.instance()
+lplot = ls(plot)
 
 
+#renderer.save(plot, 'plot')
+hv.save(plot, 'plot.html')
+#hvplot.show(plot)
 
-fig.show()
+#webbrowser.open('plot.html')
+os.system("open plot.html")
+
+#log.info("holoviews")
+#plot2 = hv.Histogram(data=traces_selected)
+#hvplot.show(plot2)
+#renderer.save(plot2, 'plot2')
+
+log.info("done")
+
